fe4fce1181182c4caf0b704a4442455e7a3cdb35
#!Roger!? 🐺 <162805518+Muhammad-H-Bakr@users.noreply.github.com>
2024-05-08 19:26:55 +0300

    Last Quiz [Quiz 2 was Written].

---
+++ DistributedLabs/Quiz3.cpp
@@ -0,0 +1,117 @@
+#include <mpich/mpi.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "MemoryLeak"
+
+double maxValue(const double mat[], int n) {
+    double max = mat[0];
+    for (int j = 0; j < n; j++) {
+        if (mat[j] > max) {
+            max = mat[j];
+        }
+    }
+    return max;
+}
+
+void printMatrix(double **mat, int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void initializeMatrix(double **&A, double *&Adata, int A_row, int A_col) {
+    Adata = (double *) malloc(sizeof(double) * A_row * A_col);
+    A = (double **) malloc(sizeof(double *) * A_row);
+    for (int i = 0; i < A_row; i++) {
+        A[i] = &(Adata[i * A_col]);
+    }
+}
+
+int main(int argc, char **argv) {
+    int rank, size;
+    int strip_size, A_row, A_col;
+    double **A, **strip_A, *Adata, *stripdata, locMax, absMax;
+
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank == 0) {
+        cout << "Enter Number of Rows: " << endl;
+        cin >> A_row;
+        cout << "Enter Number of Columns: " << endl;
+        cin >> A_col;
+
+        /* calculate the strip size */
+        strip_size = A_row / size;
+
+        /* generate Matrix A */
+        initializeMatrix(A, Adata, A_row, A_col);
+
+        // Randomizer & Seed:
+        random_device rd;
+        mt19937 gen(rd());
+
+        // Define a distribution for double values between -10.0 and 10.0 :
+        uniform_real_distribution<double> dist(-10.0, 10.0);
+        for (int i = 0; i < A_row; i++) {
+            for (int j = 0; j < A_col; j++) {
+                //Fetch the random number:
+                A[i][j] = dist(gen); //Obtain random values.
+            }
+        }
+    }
+
+    /* Begin Time Calculation */
+    MPI_Barrier(MPI_COMM_WORLD);
+    double start = MPI_Wtime();
+
+    /* Broadcasting column size of Matrix A as well as strip size */
+    MPI_Bcast(&A_col, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&strip_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    /* Initialize subMatrices */
+    initializeMatrix(strip_A, stripdata, strip_size, A_col);
+
+    /* Properly Scatter Matrix A across processors */
+    MPI_Scatter(Adata, A_col * strip_size,
+                MPI_DOUBLE, &(strip_A[0][0]), A_col * strip_size,
+                MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    // For each row; sum the absolute of its values: (Quiz Edit on Task)
+    double locSum[strip_size];
+    for (int i = 0; i < strip_size; i++) {
+        locMax = 0;
+        for (int j = 0; j < A_col; j++) {
+            locMax += abs(strip_A[i][j]);
+        }
+        locSum[i] = locMax;
+    }
+
+
+    /* Reduce & Done */
+    locMax = maxValue(locSum, strip_size); //Find maximum among sums.
+    MPI_Reduce(&locMax, &absMax, 1,
+               MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
+    /* End Time */
+    double end = MPI_Wtime();
+    if (rank == 0) {
+        cout << "Matrix: " << endl;
+        printMatrix(A, A_row, A_col);
+        cout << "Modulus: " << absMax << endl
+             << "Time: " << (end - start) * 1000 << "ms" << endl;
+    }
+    MPI_Finalize();
+    return 0;
+}
+
+#pragma clang diagnostic pop
